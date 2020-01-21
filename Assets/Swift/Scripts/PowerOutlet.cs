dd061c6baa3c07fcd856c1724434266b2fc6cd00
Treycos <maxime.poitevin@viacesi.fr>
2020-01-21 22:51:00 +0100

    Implemented product flow and interface

---
+++ Assets/Swift/Scripts/PowerOutlet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PowerOutlet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTrigger(other, true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnTrigger(other, false);
+    }
+
+    private void OnTrigger(Collider other, bool enter)
+    {
+        if (new string[] { "P", "T", "G", "F" }.Contains(other.tag))
+        {
+            Debug.Log(other.tag);
+            foreach (Transform border in other.transform.Find("FreeBorder"))
+            {
+                MeshRenderer mesh = border.GetComponent<MeshRenderer>();
+                mesh.material.color = enter ? Color.cyan : new Color() { a=.5f, r=.853f, g=.512f, b=0 };
+            }
+        }
+    }
+}
---
+++ Assets/Swift/Scripts/ProductFlowChart.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public struct ProductInfo
+{
+    public string name;
+    public Color color;
+    public List<string> machines;
+    public int coef;
+    public GameObject ui;
+}
+
+public class ProductFlowChart : MonoBehaviour
+{
+    private List<ProductInfo> ProductInfos { get; set; } = new List<ProductInfo>()
+    {
+        new ProductInfo(){ name = "A", color = Color.red, machines = new List<string>{ "F1", "F2", "T1", "P1", "F3", "G1" }, coef =  1 },
+        new ProductInfo(){ name = "B", color = Color.yellow, machines =  new List<string>{ "G2", "T3", "F2", "F4", "P2" }, coef = 5 },
+        new ProductInfo(){ name = "C", color = Color.black, machines = new List<string>{ "F3", "T2", "G1", "F4", "G2" }, coef = 3 },
+        new ProductInfo(){ name = "D", color = Color.blue, machines = new List<string>{ "F1", "T1", "F3", "F2", "G2" }, coef = 2 },
+        new ProductInfo(){ name = "E", color = Color.cyan, machines = new List<string>{ "F3", "G1", "F4", "P3" }, coef = 3 }
+    };
+
+    private List<(List<string> path, GameObject obj, string type)> CurrentProducts = new List<(List<string> path, GameObject, string)>();
+
+    public float speed;
+
+    public GameObject ProdutObject;
+    private GameObject ProductPanel;
+    public GameObject ProductInterface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProductPanel = GameObject.Find("ProductPanel");
+
+        ProductInfos = ProductInfos.Select(pInfo =>
+        {
+            StartCoroutine(CreateProduct(pInfo));
+            GameObject pInterface = Instantiate(ProductInterface, ProductPanel.transform);
+            pInfo.ui = pInterface;
+
+            pInterface.transform.Find("VisibilityButton").GetComponent<Image>().color = pInfo.color;
+            pInterface.transform.Find("PathLabel").GetComponent<Text>().text = pInfo.machines.Aggregate("", (prev, next) => prev + " > " + next);
+            pInterface.transform.Find("CoefLabel").GetComponent<Text>().text = pInfo.coef.ToString();
+
+            return pInfo;
+        }).ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newList = CurrentProducts.ToList();
+
+        foreach (var product in CurrentProducts)
+        {
+            GameObject target = GameObject.Find(product.path.First());
+            float step = speed * Time.deltaTime;
+            Vector3 targetPos = target.transform.position;
+            Vector3 currentPos = product.obj.transform.position;
+            Vector3 newPos = Vector3.MoveTowards(currentPos, targetPos, step);
+            product.obj.transform.position = newPos;
+
+            if (Vector3.Distance(product.obj.transform.position, target.transform.position) < 0.1f)
+            {
+                product.path.RemoveAt(0);
+                if(product.path.Count == 0)
+                {
+                    newList.Remove(product);
+                    Destroy(product.obj);
+                    ProductCountChange(product.type, -1);
+                }
+            }
+        }
+
+        CurrentProducts = newList;
+    }
+
+    public IEnumerator CreateProduct(ProductInfo productInfo)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(productInfo.coef);
+
+            GameObject machine = GameObject.Find(productInfo.machines.First());
+            GameObject productObject = Instantiate(ProdutObject, machine.transform);
+            productObject.GetComponent<MeshRenderer>().material.color = productInfo.color;
+
+            foreach(TextMesh tmesh in productObject.transform.GetComponentsInChildren<TextMesh>()) tmesh.text = productInfo.name;
+
+            ProductCountChange(productInfo.name, 1);
+            CurrentProducts.Add((productInfo.machines.ToList(), productObject, productInfo.name));
+        }
+    }
+
+    private void ProductCountChange(string name, int offset)
+    {
+        var t = ProductInfos.First(p => p.name == name).ui;
+        var s = t.transform.Find("AmountLabel");
+        var countLabel = ProductInfos.First(p => p.name == name).ui.transform.Find("AmountLabel").GetComponent<Text>();
+
+        countLabel.text = (int.Parse(countLabel.text) + offset).ToString();
+    }
+
+    public void SetProductVisibility(string product, bool v)
+    {
+
+    }
+}
